4d667cee0ec4fdf6bbed24f16236543f5735667c
Roman Romanov <rromanov.vk@gmail.com>
2023-10-16 23:04:51 +0500

    Bootcamp #2, Bootcamp #3

---
+++ SwiftfulThinkingBootcamp/ShapesBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ShapesBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Roman Romanov on 16.10.2023.
+//
+
+import SwiftUI
+
+struct ShapesBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapesBootcamp()
+}
---
+++ SwiftfulThinkingBootcamp/TextBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  TextBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Roman Romanov on 15.10.2023.
+//
+
+import SwiftUI
+
+struct TextBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextBootcamp()
+}
