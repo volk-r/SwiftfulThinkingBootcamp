3b67d0d667a798cece61d220cf92d00bfeac88e1
Roman Romanov <rromanov.vk@gmail.com>
2023-10-18 22:50:15 +0500

    Bootcamp #5 | Linear, Radial, and Angular Gradients in SwiftUI

---
+++ SwiftfulThinkingBootcamp/GradientsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  GradientsBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Roman Romanov on 18.10.2023.
+//
+
+import SwiftUI
+
+struct GradientsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GradientsBootcamp()
+}
