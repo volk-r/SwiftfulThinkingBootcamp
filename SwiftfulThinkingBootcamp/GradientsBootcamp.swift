c3cc35f48406ca87685ddafc0b1a177f6b532d58
Roman Romanov <rromanov.vk@gmail.com>
2023-10-18 22:51:09 +0500

    Bootcamp #5 | Linear, Radial, and Angular Gradients in SwiftUI

--- SwiftfulThinkingBootcamp/GradientsBootcamp.swift
+++ SwiftfulThinkingBootcamp/GradientsBootcamp.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct GradientsBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let firstColor = Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1))
+        let secondColor = Color(#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1))
+        RoundedRectangle(cornerRadius: 25)
+            .fill(
+//                LinearGradient(
+//                    gradient: Gradient(colors: [firstColor, secondColor]),
+//                    startPoint: .topLeading,
+//                    endPoint: .bottom)
+//                RadialGradient(
+//                    gradient: Gradient(colors: [firstColor, secondColor]),
+//                    center: .trailing,
+//                    startRadius: 5,
+//                    endRadius: 200)
+                AngularGradient.init(
+                    gradient: Gradient(colors: [firstColor, secondColor]),
+                    center: .topLeading,
+                    angle: .degrees(180 + 45))
+            )
+            .frame(width: 300, height: 200)
     }
 }
 
