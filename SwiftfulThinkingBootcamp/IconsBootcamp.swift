1843a9a163521e22e0a916dc5c819f6597d560eb
Roman Romanov <rromanov.vk@gmail.com>
2023-10-19 22:27:21 +0500

    Bootcamp #6 System Icons, Multi-Color Icons, and SF Symbols in SwiftUI

---
+++ SwiftfulThinkingBootcamp/IconsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  IconsBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Roman Romanov on 19.10.2023.
+//
+
+import SwiftUI
+
+struct IconsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconsBootcamp()
+}
