a429568bc506d7d759274db045e3dd0f4990ad8a
RRomanov <RRomanov@awardwallet.com>
2023-10-21 20:52:33 +0500

    Bootcamp #7 Adding images to a SwiftUI application

---
+++ SwiftfulThinkingBootcamp/ImageBootvamp.swift
@@ -0,0 +1,18 @@
+//
+//  ImageBootvamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Roman Romanov on 21.10.2023.
+//
+
+import SwiftUI
+
+struct ImageBootvamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageBootvamp()
+}
