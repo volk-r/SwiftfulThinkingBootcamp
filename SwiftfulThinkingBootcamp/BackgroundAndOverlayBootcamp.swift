0e97512597c8f40233c4540e3c17c213c887b724
Roman Romanov <rromanov.vk@gmail.com>
2023-10-23 22:39:15 +0500

    Bootcamp #9 Backgrounds and Overlays in SwiftUI

---
+++ SwiftfulThinkingBootcamp/BackgroundAndOverlayBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundAndOverlayBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Roman Romanov on 23.10.2023.
+//
+
+import SwiftUI
+
+struct BackgroundAndOverlayBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundAndOverlayBootcamp()
+}
