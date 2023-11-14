2164c5d68487ab40f9c75467b82f44605ceda6a8
Roman Romanov <rromanov.vk@gmail.com>
2023-11-14 23:20:13 +0500

    Bootcamp #32 How to show Alerts in SwiftUI app

--- SwiftfulThinkingBootcamp/AlertBootcamp.swift
+++ SwiftfulThinkingBootcamp/AlertBootcamp.swift
@@ -56,6 +56,9 @@ struct AlertBootcamp: View {
 //                    backgroundColor = .red
 //                }
                 Button("Ok", role: .cancel) {
+                    if alertType == .success {
+                        backgroundColor = .green
+                    }
                 }
             } message: {
                 Text(alertType?.alertDescription.message ?? "Something went wrong")
