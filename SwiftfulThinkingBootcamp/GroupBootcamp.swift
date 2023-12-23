442d71b9d1644913f13a4712cf780901616872ea
Roman Romanov <rromanov.vk@gmail.com>
2023-12-23 19:30:04 +0500

    Bootcamp #66 How to use Group in SwiftUI

---
+++ SwiftfulThinkingBootcamp/GroupBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  GroupBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Roman Romanov on 23.12.2023.
+//
+
+import SwiftUI
+
+struct GroupBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupBootcamp()
+}
